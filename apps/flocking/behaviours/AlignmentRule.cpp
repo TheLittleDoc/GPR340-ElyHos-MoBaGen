b9545b66e0a8a4cea752246cb18f20f9de51cd7e
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2026-09-06 13:24:45 -0400

    Implemented Alignment and Cohesion behaviors

--- apps/flocking/behaviours/AlignmentRule.cpp
+++ apps/flocking/behaviours/AlignmentRule.cpp
@@ -4,9 +4,21 @@
 glm::vec2 AlignmentRule::computeForce(const std::vector<BoidView>& neighborhood, const BoidView& boid) {
   glm::vec2 averageVelocity(0.f);
   // glm::vec2 can be divided by a float, which will divide each component of the vector by that float.
-
+  averageVelocity = boid.velocity;
   // begin solution
+  if (neighborhood.size() == 1) {
+    averageVelocity += neighborhood.front().velocity;
+    averageVelocity /= 2;
+    return averageVelocity;
+  } else if (neighborhood.size() == 0) {
+    return {};
+  }
 
+  for (auto nboid : neighborhood) {
+    averageVelocity += nboid.velocity;
+  }
+
+  averageVelocity /= (neighborhood.size() + 1);
 
   return averageVelocity;
   // end solution
