03e1e5ab5f0e0b26231afeef755438af192b2cb6
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2026-09-06 22:39:00 -0400

    Added rough Bounded Window behavior

--- apps/flocking/behaviours/BoundedAreaRule.cpp
+++ apps/flocking/behaviours/BoundedAreaRule.cpp
@@ -7,9 +7,32 @@ glm::vec2 BoundedAreaRule::computeForce(const std::vector<BoidView>& neighborhoo
   glm::vec2 force(0.f);
   ImVec2 displaySize = ImGui::GetIO().DisplaySize;
   // desiredDistance is the distance from the borders that the boids should try to maintain. 
-
+  glm::vec2 normal = {};
   // begin solution
+  glm::vec2 futurePos = boid.velocity * 0.0167f + boid.position;
+  //early return if future pos is in box
+  if (futurePos.x >= desiredDistance && futurePos.x <= displaySize.x - desiredDistance &&
+      futurePos.y >= desiredDistance && futurePos.y <= displaySize.y - desiredDistance) {
+    return {};
+  }
 
+  // nearest border
+  if (futurePos.x < desiredDistance) {
+    normal = glm::vec2(1.f, 0.f);
+  } else if (futurePos.x > displaySize.x - desiredDistance) {
+    normal= glm::vec2(-1.f, 0.f);
+  } else if (futurePos.y < desiredDistance) {
+    normal = glm::vec2(0.f, 1.f);
+  } else if (futurePos.y > displaySize.y - desiredDistance) {
+    normal = glm::vec2(0.f, -1.f);
+  }
+  // when a border is approached, a center-pointing force is applied
+  glm::vec2 centerDir = boid.position - glm::vec2(displaySize.x / 2.f, displaySize.y / 2.f);
+  if (glm::length(centerDir) > 0.f) {
+    centerDir = glm::normalize(centerDir);
+    force += -centerDir * (desiredDistance * 4 / glm::length(centerDir));
+  }
+  force -= normal;
   // end solution
 
   return force;
