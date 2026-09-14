8b22d5980bcbcfcebfd694c59b39962a5ad34518
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2026-09-14 00:43:32 -0400

    Started implementing GoL

--- apps/life/rules/JohnConway.cpp
+++ apps/life/rules/JohnConway.cpp
@@ -6,6 +6,35 @@ void JohnConway::Step(World& world) {
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-  // todo: implement
-  return 0;
+  // Eight neighbors
+  Point2D checkedPoint = point;
+  int neighbors = 0;
+  checkedPoint.x--;
+  checkedPoint.y--;
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      if (x == 1 && y == 1) continue;
+      Point2D addPoint = {x, y};
+      if (checkedPoint.x + x < 0)
+      {
+        addPoint.x = world.SideSize() - 1;
+      }
+      if (checkedPoint.y + y < 0) {
+        addPoint.y = world.SideSize() - 1;
+      }
+
+      if (checkedPoint.x + x + 1 >= world.SideSize()) {
+        addPoint.x = -world.SideSize() + 1;
+      }
+      if (checkedPoint.y + y + 1 >= world.SideSize()) {
+        addPoint.y = -world.SideSize() + 1;
+      }
+
+      if (world.Get(checkedPoint + addPoint))
+      {
+        neighbors++;
+      }
+    }
+  }
+  return neighbors;
 }
